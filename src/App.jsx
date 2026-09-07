2ae88af3e14a82bb3a4ace09514d4679444347a9
dineshgnanavel01-droid <dineshgnanavel01@gmail.com>
2026-09-07 17:43:53 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import InteractiveBackground from "./components/InteractiveBackground";
+
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import WatchDetails from "./pages/WatchDetails";
@@ -10,31 +12,76 @@ import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+
 import { CartProvider } from "./context/CartContext";
-import InteractiveBackground from "./components/InteractiveBackground";
-// Removed the incorrect "../../animations" import from here!
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2800);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <CartProvider>
       <BrowserRouter>
-        <InteractiveBackground>
-          <Navbar />
 
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/collection" element={<Collection />} />
-              <Route path="/watch/:id" element={<WatchDetails />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/checkout" element={<Checkout />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-            </Routes>
-          </main>
+       
+        <div className={loading ? "app-hidden" : "app-visible"}>
+          <InteractiveBackground>
 
-          <Footer />
-        </InteractiveBackground>
+            <Navbar />
+
+            <main className="flex-grow">
+              <Routes>
+
+                <Route
+                  path="/"
+                  element={<Home />}
+                />
+
+                <Route
+                  path="/collection"
+                  element={<Collection />}
+                />
+
+                <Route
+                  path="/watch/:id"
+                  element={<WatchDetails />}
+                />
+
+                <Route
+                  path="/cart"
+                  element={<Cart />}
+                />
+
+                <Route
+                  path="/checkout"
+                  element={<Checkout />}
+                />
+
+                <Route
+                  path="/login"
+                  element={<Login />}
+                />
+
+                <Route
+                  path="/signup"
+                  element={<Signup />}
+                />
+
+              </Routes>
+            </main>
+
+            <Footer />
+
+          </InteractiveBackground>
+        </div>
+
       </BrowserRouter>
     </CartProvider>
   );
