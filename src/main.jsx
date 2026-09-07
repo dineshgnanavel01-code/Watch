7c90a0571a0c03c45c92b9556355aaeafb4a7efd
dineshgnanavel01-code <dineshgnanavel01@gmail.com>
2026-09-07 11:08:09 +0530

    fix: remove duplicate CartProvider wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { CartProvider } from './context/CartContext'; // Ensure path is correct
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <App />
   </React.StrictMode>
 );
